68848ba99e3f88671422c696a438e941f688792a
Toan Vu <toanvu@Toans-MacBook-Air.local>
2016-04-05 21:35:37 +0700

    init wi vendor

---
+++ src/GenericDAO/Category.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @package wibi
+ * authod <vdtoan.au@gmail.com>
+ */
+namespace GenericDAO;
+
+class Category
+{
+	/*
+	 * @var int 
+	 */
+	private $_category_id;
+}
---
+++ vendor/wi/GenericDAO.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @package wibi
+ * authod <vdtoan.au@gmail.com>
+ */
+
+namespace Wi;
+
+use Zend\Db\Adapter\Adapter as ZendAdapter;
+
+class GenericDAO
+{
+	/*
+	 * @var array
+	 */
+	
+}
